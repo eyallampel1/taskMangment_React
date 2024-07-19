8000dc14bff310e9f938b7b894ac70b2bc76c38b
Eyal Lampel <eyal.lampel@gmail.com>
2024-07-19 16:08:12 +0300

    Added active button color function

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,6 +6,7 @@ export function TaskList({ task }) {
     const [taskArray, setTaskArray] = useState([]);
     const [completedTasks, setCompletedTasks] = useState({});
     const [idCounter, setIdCounter] = useState(0);
+    const [filter, setFilter] = useState("All");
 
     const generateUniqueId = () => {
         setIdCounter(prevIdCounter => prevIdCounter + 1);
@@ -40,17 +41,42 @@ export function TaskList({ task }) {
         setTaskArray((prevTaskArray) => [...prevTaskArray, newTask]);
     }, [task]);
 
+    const getFilteredTasks = () => {
+        if (filter === "Completed") {
+            return taskArray.filter(task => completedTasks[task.id]);
+        } else if (filter === "In Progress") {
+            return taskArray.filter(task => !completedTasks[task.id]);
+        } else {
+            return taskArray;
+        }
+    };
+
     return (
         <div className="bg-red-400 mt-3 mb-3">
             <div className="text-3xl">TaskList</div>
 
             <div className="flex justify-between m-4">
-                <button className="bg-gray-200 text-gray-700 p-2 rounded hover:bg-gray-100 mb-3">Completed</button>
-                <button className="bg-gray-200 text-gray-700 p-2 rounded hover:bg-gray-100 mb-3">In Progress</button>
-                <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-300 mb-3">All</button>
+                <button
+                    onClick={() => setFilter("Completed")}
+                    className={`p-2 rounded mb-3 ${filter === "Completed" ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-700 hover:bg-gray-100"}`}
+                >
+                    Completed
+                </button>
+                <button
+                    onClick={() => setFilter("In Progress")}
+                    className={`p-2 rounded mb-3 ${filter === "In Progress" ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-700 hover:bg-gray-100"}`}
+                >
+                    In Progress
+                </button>
+                <button
+                    onClick={() => setFilter("All")}
+                    className={`p-2 rounded mb-3 ${filter === "All" ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-700 hover:bg-gray-100"}`}
+                >
+                    All
+                </button>
             </div>
             <ul>
-                {taskArray.map(({ id, text }) => (
+                {getFilteredTasks().map(({ id, text }) => (
                     <li key={id} className="mb-3">
                         <TaskItem
                             task={text}
