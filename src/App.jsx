497110d056c9098e5426b9f09df3905ea7fae24a
Eyal Lampel <eyal.lampel@gmail.com>
2024-07-19 15:36:54 +0300

    Fixed empty rows bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import {TaskForm} from "./components/TaskForm.jsx";
-import {TaskList} from "./components/TaskList.jsx";
-import {useState} from "react";
+import { TaskForm } from "./components/TaskForm.jsx";
+import { TaskList } from "./components/TaskList.jsx";
+import { useState } from "react";
 
 function App() {
-    const [task, setTask] = useState([])
-  return (
- <>
-     <div className={"flex justify-center mb-3 underline"}>
-         <div className={"text-3xl"}>Task Management</div>
-     </div>
-         <TaskForm setTask={setTask}/>
-     <TaskList task={task} />
- </>
-  )
+    const [task, setTask] = useState("");
+
+    return (
+        <>
+            <div className="flex justify-center mb-3 underline">
+                <div className="text-3xl">Task Management</div>
+            </div>
+            <TaskForm setTask={setTask} />
+            <TaskList task={task} />
+        </>
+    );
 }
 
-export default App
+export default App;
